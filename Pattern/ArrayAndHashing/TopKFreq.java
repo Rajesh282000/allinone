691859f1d7e6dbdbc34d0d034a70fa8b38e888ec
Rajesh282000 <rajeshburnwal28112000@gmail.com>
2025-08-01 19:54:31 +0530

    Top K Freq using PriorityQueue (nlogn)

--- Pattern/ArrayAndHashing/TopKFreq.java
+++ Pattern/ArrayAndHashing/TopKFreq.java
@@ -44,6 +44,39 @@ public class TopKFreq {
         return arr;
 
     }
+
+    public int[] topKFrequentPriotityQueue(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequencies
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+        // Use a max heap (priority queue) sorted by frequency
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
+                (a, b) -> b.getValue() - a.getValue()
+        );
+
+        // Add all entries to the max heap
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxHeap.add(entry);
+        }
+
+        // Extract the top k elements
+        int[] arr = new int[k];
+        int j = 0;
+        while (j < k && !maxHeap.isEmpty()) {
+            arr[j++] = maxHeap.poll().getKey();
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
 
     }
