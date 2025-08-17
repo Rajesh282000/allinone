a81b604e15f06d5c62b7b31f004c211bddaaf824
Rajesh282000 <rajeshburnwal28112000@gmail.com>
2025-08-17 16:02:09 +0530

    Implementation of BinarySearchTree

---
+++ Tree/BinarySearchTree.java
@@ -0,0 +1,106 @@
+package Tree;
+
+public class BinarySearchTree {
+
+    private class Node {
+        int value;
+        int height;
+        Node left;
+        Node right;
+        Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Node root;
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    BinarySearchTree() {
+
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    private Node insert(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
+        if (value < node.value) {
+            node.left = insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = insert(node.right, value);
+        }
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        return node;
+    }
+
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return  Math.abs(leftHeight - rightHeight) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String indent) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(indent + node.value);
+        display(node.left, "Left child of "+ node.value + ": ");
+        display(node.right, "right child of "+ node.value + ": ");
+    }
+    public void populate(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            insert(nums[i]);
+        }
+    }
+
+    public void populateSorted(int[] nums){
+        populateSorted(nums, 0, nums.length);
+
+    }
+    private void populateSorted(int[] nums, int start, int end) {
+       while(start <= end){
+           return ;
+       }
+
+       int mid = (start + end) / 2;
+       insert(nums[mid]);
+       populateSorted(nums, start, mid - 1);
+       populateSorted(nums, mid + 1, end);
+    }
+
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        int[] arr = {5, 3, 7, 2, 4, 6, 8, 1, 9};
+        bst.populate(arr);
+        bst.display();
+    }
+}
